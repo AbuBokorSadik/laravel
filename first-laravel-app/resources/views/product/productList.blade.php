304acd9acb3bbf8b634df55c09773e0d8c8169c5
Abu Bokor Sadik <sadikabubokor@gmail.com>
2021-06-24 18:19:09 +0600

    first commit

---
+++ first-laravel-app/resources/views/product/productList.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Product list page</h1>
+    <p>
+        @php
+        echo '<pre>';
+
+        print_r($users->toArray());
+
+        exit();
+        @endphp
+    </p>
+</body>
+</html>
