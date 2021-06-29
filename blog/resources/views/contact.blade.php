78056c909a6ae14a14b87e30e61509950a86cae3
Abu Bokor Sadik <sadikabubokor@gmail.com>
2021-06-29 11:34:28 +0600

    first commit

--- blog/resources/views/contact.blade.php
+++ blog/resources/views/contact.blade.php
@@ -1,23 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
 <body>
-    <h1>Product list page</h1>
-    <p>
-        @php
-        echo '<pre>';
-
-        print_r($users->toArray());
-
-        exit();
-        @endphp
-    </p>
+    <h1>contact page</h1>
 </body>
 </html>
