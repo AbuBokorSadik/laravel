e10fe3ec457c84f604f3ca2d35e5e6d351fecbde
Abu Bokor Sadik <sadikabubokor@gmail.com>
2021-10-17 14:58:46 +0600

    api

---
+++ Laravel-Library-API/app/Http/Controllers/API/V1/Book/BookController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers\API\V1\Book;
+
+use App\Http\Controllers\Controller;
+use App\Models\Book;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\ValidationException;
+use App\Traits\ApiResponseTraits;
+
+class BookController extends Controller
+{
+    use ApiResponseTraits;
+
+    public function index()
+    {
+        try {
+            $book = Book::paginate(15);
+
+            return $this->respondInJSON(200, ['success'], $book);
+        } catch (\Exception $e) {
+            Log::error($e->getFile() . ' ' . $e->getLine() . ' ' . $e->getMessage());
+
+            return $this->failedResponse(500, ['Failed'], null);
+        }
+    }
+
+    public function create(Request $request)
+    {
+        try {
+            $validation = Validator::make($request->all(), [
+                'name' => 'required|unique:books',
+                'writer' => 'required',
+                'publication' => 'required',
+            ]);
+
+            if ($validation->fails()) {
+                throw new ValidationException($validation);
+            }
+
+            $book = Book::create([
+                'name' => $request->name,
+                'writer' => $request->writer,
+                'publication' => $request->publication
+            ]);
+
+            return $this->respondInJSON(200, ['success'], $book);
+        } catch (ValidationException $e) {
+            Log::error($e->getFile() . ' ' . $e->getLine() . ' ' . $e->getMessage());
+
+            return $this->failedResponse(422, [], $e, true);
+        } catch (\Exception $e) {
+            Log::error($e->getFile() . ' ' . $e->getLine() . ' ' . $e->getMessage());
+
+            return $this->failedResponse(500, ['Failed'], null);
+        }
+    }
+
+    public function update($id, Request $request)
+    {
+        try {
+            $book = Book::find($id);
+
+            if(!$book){
+                return $this->failedResponse(404, ['Book not found'], null);
+            }
+
+            $validation = Validator::make($request->all(), [
+                'name' => ['required', Rule::unique('books', 'name')->ignore($book->id)],
+                'writer' => 'required',
+                'publication' => 'required',
+            ]);
+
+            if ($validation->fails()) {
+                throw new ValidationException($validation);
+            }
+
+            $book->name = $request->name;
+            $book->writer = $request->writer;
+            $book->publication = $request->publication;
+            $book->save();
+
+            return $this->respondInJSON(200, ['success'], $book);
+        } catch (ValidationException $e) {
+            Log::error($e->getFile() . ' ' . $e->getLine() . ' ' . $e->getMessage());
+
+            return $this->failedResponse(422, [], $e, true);
+        } catch (\Exception $e) {
+            Log::error($e->getFile() . ' ' . $e->getLine() . ' ' . $e->getMessage());
+
+            return $this->failedResponse(500, ['Failed'], null);
+        }
+    }
+
+    public function delete($id)
+    {
+        try {
+            $book = Book::find($id);
+
+            if(!$book){
+                return $this->failedResponse(404, ['Book not found'], null);
+            }
+            
+            $book->delete();
+
+            return $this->respondInJSON(200, ['success'], $book);
+        } catch (\Exception $e) {
+            Log::error($e->getFile() . ' ' . $e->getLine() . ' ' . $e->getMessage());
+
+            return $this->failedResponse(500, ['Failed'], null);
+        }
+    }
+}
--- Laravel-Library-API/routes/api.php
+++ Laravel-Library-API/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\V1\Book\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::group(['prefix' => 'v1', 'namespace' => 'API\\V1\\Book'], function(){
+    Route::get('books', [BookController::class, 'index']);
+    Route::post('books', [BookController::class, 'create']);
+    Route::put('books/{id}', [BookController::class, 'update']);
+    Route::delete('books/{id}', [BookController::class, 'delete']);
+
 });
